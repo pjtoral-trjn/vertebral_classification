2314271af74dc90ee9a1d3743d2bcd79b0d8086e
PT <pjtoral@usc.edu>
2025-08-30 21:49:43 +0000

    Figures, Notebooks and plot helper changes

--- src/plot_helper.py
+++ src/plot_helper.py
@@ -13,3 +13,36 @@ def plot_label_distribution(df, df_pos, df_neg, save_path="./", file_name="label
     plt.xticks([0,1])
     plt.savefig(save_path+"/"+file_name)
     plt.show()
+
+
+def plot_scatter(df_pos, df_neg, independent_variables,save_path="./", file_name="scatter.png"):
+    fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(14,8))
+    for i, var in enumerate(independent_variables):
+        pos = sorted(df_pos[var].to_numpy())
+        neg = sorted(df_neg[var].to_numpy())
+        x_pos = np.arange(len(pos))
+        x_neg = np.arange(len(pos), len(pos) + len(neg))
+        if i < 3:
+            axs = axes[0,i]
+        else:
+            axs = axes[1,i-3]
+        axs.scatter(x_pos, pos, label='1 (Abnormal)', alpha=0.7)
+        axs.scatter(x_neg, neg, label='0 (Normal)', alpha=0.7)
+        axs.set_title(var)
+    axes[0,1].legend()
+    plt.savefig(save_path+file_name)
+    plt.show()
+
+
+def plot_boxplot(df,independent_variables,save_path="./", file_name="boxplot.png"):
+    fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(14,8))
+    for i, var in enumerate(independent_variables):
+        if i < 3:
+            axs = axes[0,i]
+        else:
+            axs = axes[1,i-3]
+        sns.boxplot(data=df, x="class", y=var, ax=axs)
+    axes[0,1].legend()
+    plt.savefig(save_path+file_name)
+    plt.show()
+
